6a21d1605cab9c2a286127f9194e3ca0f09267e0
Rolland Jolan <o22402457@or-iut-i207-f04.iut45.univ-orleans.fr>
2025-09-03 15:29:13 +0200

    Fin de séance 1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PROJET_FLASK
+Rolland
+Jolan
+
+Création de l'environnement 
+pip freeze > requirements.txt
---
+++ monApp/views.py
@@ -0,0 +1,6 @@
+from .app import app
+@app.route('/')
+def index():
+    return "Hello world !"
+if __name__ == "__main__":
+    app.run()
