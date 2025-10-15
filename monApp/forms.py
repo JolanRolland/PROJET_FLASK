9f0d90ef467049083a5e3b43ed5118b72e167ce6
JolanRolland <jolan.rolland@etu.univ-orleans.fr>
2025-10-15 20:46:34 +0200

    Fin du tp5

--- monApp/forms.py
+++ monApp/forms.py
@@ -1,5 +1,6 @@
+from decimal import ROUND_HALF_UP
 from flask_wtf import FlaskForm
-from wtforms import FloatField, StringField, HiddenField
+from wtforms import DecimalField, StringField, HiddenField
 from wtforms.validators import DataRequired
 class FormAuteur(FlaskForm):
     idA=HiddenField('idA')
@@ -7,11 +8,13 @@ class FormAuteur(FlaskForm):
 
 class FormLivre(FlaskForm):
     idL = HiddenField('idL')
-    Titre = StringField('Titre', validators=[DataRequired()])
-    AuteurId = HiddenField('AuteurId') 
-    
-    Prix = FloatField(
-        'Prix', 
+    Titre = StringField('Titre')  # mets-le en readonly dans le template si non modifiable
+    AuteurId = HiddenField('AuteurId')
+
+    Prix = DecimalField(
+        'Prix',
+        places=2,
+        rounding=ROUND_HALF_UP,
         validators=[
             DataRequired()
         ]
